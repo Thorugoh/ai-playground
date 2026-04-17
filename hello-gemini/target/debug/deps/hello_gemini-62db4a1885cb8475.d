3abab8ef83dd9de42f4a5c06f4fe1f7272cd6cd6
Victor Hugo <hugo.victoor77@gmail.com>
2026-04-17 08:53:44 -0300

    create gemini project

---
+++ hello-gemini/target/debug/deps/hello_gemini-62db4a1885cb8475.d
@@ -0,0 +1,5 @@
+/Users/vhugo/repos/ai-playground/hello-gemini/target/debug/deps/hello_gemini-62db4a1885cb8475.d: src/main.rs
+
+/Users/vhugo/repos/ai-playground/hello-gemini/target/debug/deps/libhello_gemini-62db4a1885cb8475.rmeta: src/main.rs
+
+src/main.rs:
---
+++ hello-gemini/target/debug/deps/hello_gemini-ed349c9938d01e29.d
@@ -0,0 +1,5 @@
+/Users/vhugo/repos/ai-playground/hello-gemini/target/debug/deps/hello_gemini-ed349c9938d01e29.d: src/main.rs
+
+/Users/vhugo/repos/ai-playground/hello-gemini/target/debug/deps/libhello_gemini-ed349c9938d01e29.rmeta: src/main.rs
+
+src/main.rs:
